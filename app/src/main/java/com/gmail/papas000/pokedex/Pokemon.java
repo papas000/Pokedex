177a42cbba2284d270a9fb8cc6c67096f76a61f5
papas000 <papas000@gmail.com>
2020-04-24 17:44:38 +0200

    Initial commit

---
+++ app/src/main/java/com/gmail/papas000/pokedex/Pokemon.java
@@ -0,0 +1,19 @@
+package com.gmail.papas000.pokedex;
+
+public class Pokemon {
+    private String name;
+    private String url;
+
+    Pokemon(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
